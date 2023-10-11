62863db1c8d66c1af33c10e3a3a34cfccbd51187
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-10-11 14:29:40 +0800

    feat: update test

--- packages/core/src/test.ts
+++ packages/core/src/test.ts
@@ -1,6 +1,11 @@
 import { GZHULibraryBookingManagerImpl } from '../dist/index.js'
 
-const gzhuLibraryBookingManagerImpl = new GZHULibraryBookingManagerImpl()
-const { loginSuccessCookieValue } = await gzhuLibraryBookingManagerImpl.login('123', 'abc')
+const username = process.env.GZHU_USERNAME
+const password = process.env.GZHU_PASSWORD
 
-console.log(loginSuccessCookieValue)
+if (username && password) {
+  const gzhuLibraryBookingManagerImpl = new GZHULibraryBookingManagerImpl()
+  const { loginSuccessCookieValue } = await gzhuLibraryBookingManagerImpl.login(username, password)
+
+  console.log(loginSuccessCookieValue)
+}
