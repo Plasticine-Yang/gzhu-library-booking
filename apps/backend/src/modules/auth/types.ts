a8fd08ba13ce688c0028496be98e3a66d78f86d8
plasticine <975036719@qq.com>
2023-10-17 22:54:15 +0800

    feat(backend): 获取用户信息接口接入 jwt

--- apps/backend/src/modules/auth/types.ts
+++ apps/backend/src/modules/auth/types.ts
@@ -1,3 +1,6 @@
+import { Request } from 'express'
 import { User } from '../user/entities/user.entity'
 
 export type JwtUserPayload = Pick<User, 'id' | 'username'>
+
+export type RequestWithJwtUserPayload = Request & { user: JwtUserPayload }
--- apps/backend/src/modules/user/user.controller.ts
+++ apps/backend/src/modules/user/user.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Req, UseGuards } from '@nestjs/common'
 
+import { AuthGuard } from '../auth/auth.guard'
+import { RequestWithJwtUserPayload } from '../auth/types'
 import { UserSelector } from './enums'
 import { UserService } from './user.service'
 
@@ -7,8 +9,11 @@ import { UserService } from './user.service'
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get(':username')
-  findOne(@Param('username') username: string) {
-    return this.userService.findOne({ username }, UserSelector.Normal)
+  @Get()
+  @UseGuards(AuthGuard)
+  findOne(@Req() req: RequestWithJwtUserPayload) {
+    const { id } = req.user
+
+    return this.userService.findOne({ id }, UserSelector.Normal)
   }
 }
