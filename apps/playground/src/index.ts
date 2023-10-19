bfcbba449c4b68a294755e79e60c672fc94aa5b3
plasticine <975036719@qq.com>
2023-10-20 00:17:37 +0800

    feat(playground): update

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,29 +1,25 @@
 import { run } from '@gzhu-library-booking/core'
 
-const username = process.env.GZHU_USERNAME
-const password = process.env.GZHU_PASSWORD
-
-if (username && password) {
-  run({
-    username,
-    password,
-    mailerOptions: {
-      host: 'smtp.qq.com',
-      port: 465,
-      user: '你的邮箱',
-      pass: '你的邮箱授权码',
-      from: '发送方邮箱',
-      to: '接收方邮箱',
+run({
+  username: '数字广大用户名',
+  password: '数字广大密码',
+  mailerOptions: {
+    host: 'smtp.qq.com',
+    port: 465,
+    user: '你的邮箱',
+    pass: '你的邮箱授权码',
+    from: '发件人邮箱',
+    to: '收件人邮箱',
+  },
+  rules: [
+    {
+      beginTime: '8:30:00',
+      endTime: '12:00:00',
+      dayDelta: 3,
+      roomName: 'E21',
+      reserverStudentIdList: ['你的学号'],
+      runWhenReady: true,
     },
-    rules: [
-      {
-        beginTime: '8:30:00',
-        endTime: '12:00:00',
-        dayDelta: 3,
-        roomName: 'E25',
-        reserverStudentIdList: ['预约人学号'],
-        initiateReserveTime: '6:30:00',
-      },
-    ],
-  })
-}
+  ],
+  puppeteerOptions: { noSandBox: true, openBrowser: false },
+})
